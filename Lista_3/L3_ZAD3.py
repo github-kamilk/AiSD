c59d986700a1bb924fe219f76fb4aaa71e3c6c3a
github-kamilk <84456262+github-kamilk@users.noreply.github.com>
2021-11-16 14:54:13 +0100

    Update L3_ZAD3.py

--- Lista_3/L3_ZAD3.py
+++ Lista_3/L3_ZAD3.py
@@ -1,1 +1,10 @@
+def counting_chars_without_ifs(filename):
+    file_ref = open(filename, 'r')
+    text = file_ref.read()
+    text = text.replace(" ", "").lower()
 
+    char_count = {k: 0 for k in text}
+    for i in text:
+        char_count[i] += 1
+
+    return char_count
