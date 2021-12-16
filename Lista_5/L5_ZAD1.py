1b42f55e3033f3f2dbe798f6fedbd5d0fc9f8a09
github-kamilk <kamil.kowalski51@onet.pl>
2021-12-16 15:45:46 +0100

    Update L5_ZAD1.py

--- Lista_5/L5_ZAD1.py
+++ Lista_5/L5_ZAD1.py
@@ -1,16 +1,28 @@
 import numpy as np
 from scipy import linalg
+import random
 import time
 
-a = np.array([[3, 2, 0], [1, -1, 0], [0, 5, 1]])
-b = np.array([2, 4, -1])
 
-x = linalg.solve(a, b)
-print(x)
+def create_matrix(n):
+    a = np.array([[random.randint(-100, 100) for _ in range(n)] for _ in range(n)])
+    b = np.array([random.randint(-100, 100) for _ in range(n)])
+    return linear_solving(a, b)
 
-def linear_solving(a,b):
+
+def linear_solving(a, b):
     x = linalg.solve(a, b)
     return x
 
+
 def time_checker(n):
-    pass
+    execution_times = []
+    for i in range(1, n):
+        start = time.time()
+        create_matrix(i)
+        stop = time.time()
+        execution_times.append(stop - start)
+    return execution_times
+
+
+print(time_checker(1000))
