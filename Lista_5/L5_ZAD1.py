81c30a19d54c5f7131734ad9ae007f867a92e230
github-kamilk <kamil.kowalski51@onet.pl>
2021-12-16 18:11:24 +0100

    Update L5_ZAD1.py

--- Lista_5/L5_ZAD1.py
+++ Lista_5/L5_ZAD1.py
@@ -2,6 +2,8 @@ import numpy as np
 from scipy import linalg
 import random
 import time
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 
 def create_matrix(n):
@@ -16,13 +18,50 @@ def linear_solving(a, b):
 
 
 def time_checker(n):
+    start = time.time()
+    create_matrix(n)
+    stop = time.time()
+    return stop - start
+
+
+def func(x, a, b):
+    return a * x ** 2 + b
+
+def plot(x, y):
+    plt.plot(x, y, 'ro', label="Dane")
+    plt.xlabel("Liczba niewiadomych")
+    plt.ylabel("Czas wykonania [s]")
+    plt.legend(loc='upper left')
+    plt.title("Wykres zależności czasu od ilości niewiadomych")
+    plt.show()
+
+
+def log_plot(x, y):
+    plt.loglog(x, y, 'ro', label="Dane")
+    plt.xlabel("Liczba niewiadomych")
+    plt.ylabel("Czas wykonania [s]")
+    plt.legend(loc='upper left')
+    plt.title("Wykres zależności czasu od ilości niewiadomych")
+    plt.show()
+
+def hypothesis_plot(x, y, popt):
+    x2 = np.arange(1, x[-1])
+
+    plt.plot(x, y, 'ro', label="Dane")
+    plt.plot(x2, func(x2, *popt), label="Hipoteza")
+    plt.xlabel("Liczba niewiadomych")
+    plt.ylabel("Czas wykonania [s]")
+    plt.legend(loc='upper left')
+    plt.title("Wykres zależności czasu od ilości niewiadomych")
+    plt.show()
+
+
+if __name__ == "__main__":
+    n = [2 ** i for i in range(1, 14)]
     execution_times = []
-    for i in range(1, n):
-        start = time.time()
-        create_matrix(i)
-        stop = time.time()
-        execution_times.append(stop - start)
-    return execution_times
+    for i in n:
+        execution_times.append(time_checker(i))
 
-
-print(time_checker(1000))
+    # log_plot(n, execution_times)
+    popt, pcov = curve_fit(func, n, execution_times)
+    hypothesis_plot(n, execution_times,popt)
