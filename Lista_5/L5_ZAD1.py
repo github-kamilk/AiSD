a8a93c24f572af6d4e62b40ec67d0799f3cb4570
github-kamilk <kamil.kowalski51@onet.pl>
2021-12-21 20:44:02 +0100

    Update L5_ZAD1.py

--- Lista_5/L5_ZAD1.py
+++ Lista_5/L5_ZAD1.py
@@ -4,29 +4,23 @@ import random
 import time
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-import math
+import csv
 
 
-def create_matrix(n):
-    a = np.array([[random.randint(-100, 100) for _ in range(n)] for _ in range(n)])
-    b = np.array([random.randint(-100, 100) for _ in range(n)])
-    return linear_solving(a, b)
+class Fit_class:
+    def __init__(self):
+        pass
 
-
-def linear_solving(a, b):
-    x = linalg.solve(a, b)
-    return x
-
+    def fit_fun(self, x, a):
+        return a * x ** self.power
 
 def time_checker(n):
+    a = np.array([[random.randint(-100, 100) for _ in range(n)] for _ in range(n)])
+    b = np.array([random.randint(-100, 100) for _ in range(n)])
     start = time.time()
-    create_matrix(n)
+    linalg.solve(a, b)
     stop = time.time()
     return stop - start
-
-
-def func(x, a, b, power=2):
-    return a * x ** power + b
 
 
 def plot(x, y):
@@ -38,16 +32,8 @@ def plot(x, y):
     plt.show()
 
 
-def log_plot(x, y):
-    plt.loglog(x, y, 'ro', label="Dane")
-    plt.xlabel("Liczba niewiadomych")
-    plt.ylabel("Czas wykonania [s]")
-    plt.legend(loc='upper left')
-    plt.title("Wykres czasu w zależności od ilości niewiadomych")
-    plt.show()
 
-
-def hypothesis_plot(x, y, popt):
+def hypothesis_plot(x, y, func, popt):
     x2 = np.arange(1, x[-1])
 
     plt.plot(x, y, 'ro', label="Dane")
@@ -59,34 +45,25 @@ def hypothesis_plot(x, y, popt):
     plt.show()
 
 
-def doubling(x_tab, y_tab):
-    ratio = [None] * len(y_tab)
-    for i in range(1, len(y_tab)):
-        if y_tab[i - 1] != 0:
-            ratio[i] = y_tab[i] / y_tab[i - 1]
-        else:
-            ratio[i] = 0
-    lratio = [None]
-    for val in ratio:
-        if val:
-            lratio.append(math.log(val, 2))
-        else:
-            lratio.append(None)
-
-    print("{} \t {} \t {} \t {}".format("N", "T", "Ratio", "Log"))
-    for i in range(len(y_tab)):
-        print("{} \t {} \t {} \t {}".format(x_tab[i], y_tab[i], ratio[i], lratio[i]))
-
-
 if __name__ == "__main__":
-    n = [2 ** i for i in range(1, 11)]
-    execution_times = []
-    for i in n:
-        execution_times.append(time_checker(i))
+    n = [1000*i for i in range(1,14)]
 
-    # plot(n, execution_times)
-    # log_plot(n, execution_times)
-    popt, pcov = curve_fit(func, n, execution_times)
-    print(popt)
-    doubling(n, execution_times)
-    hypothesis_plot(n, execution_times, popt)
+    execution_times = []
+    # for i in n:
+    #     execution_times.append(time_checker(i))
+    #
+    # with open('execution_times', 'w', newline='') as myfile:
+    #     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    #     wr.writerow(execution_times)
+
+    with open('execution_times.csv', newline='') as f:
+        reader = csv.reader(f)
+        execution_times = [float(i) for i in list(reader)[0]]
+
+    inst = Fit_class()
+    inst.power = np.polyfit(np.log(n), np.log(execution_times),1)[0]
+
+    plot(n, execution_times)
+    plot(np.log(n), np.log(execution_times))
+    popt, pcov = curve_fit(inst.fit_fun, n, execution_times)
+    hypothesis_plot(n, execution_times,inst.fit_fun, popt)
