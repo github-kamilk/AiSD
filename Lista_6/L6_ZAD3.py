adab6d3a7e20c34f4e7403f90d716215a7975014
malgorzata-k <84721844+malgorzata-k@users.noreply.github.com>
2022-01-18 14:03:42 +0100

    Update L6_ZAD3.py

--- Lista_6/L6_ZAD3.py
+++ Lista_6/L6_ZAD3.py
@@ -4,7 +4,7 @@ class BinHeap:
         self.current_size = 0
         self.max_size = max_size
 
-    def percUp(self,index):
+    def perc_up(self,index):
         while index // 2 > 0:
             if self.heap_list[index] < self.heap_list[index // 2]:
                 tmp = self.heap_list[index // 2]
@@ -14,30 +14,30 @@ class BinHeap:
         
     def insert(self,value):
         if self.current_size == self.max_size:
-            if value > self.findMin():
+            if value > self.find_min():
                 self.heap_list.append(value)
-                self.delMin()
+                self.del_min()
                 self.current_size += 1
             else:
                 raise ValueError("The value is too small to put on the Binary Heap")
         else: 
             self.heap_list.append(value)
             self.current_size += 1
-            self.percUp(self.current_size)        
+            self.perc_up(self.current_size)        
         
-    def findMin(self):
+    def find_min(self):
         return self.heap_list[1]
 
-    def percDown(self,index):
+    def perc_down(self,index):
         while (index * 2) <= self.current_size:
-            mc = self.minChild(index)
+            mc = self.min_child(index)
             if self.heap_list[index] > self.heap_list[mc]:
                 tmp = self.heap_list[index]
                 self.heap_list[index] = self.heap_list[mc]
                 self.heap_list[mc] = tmp
             index = mc
 
-    def minChild(self,index):
+    def min_child(self,index):
         if index * 2 + 1 > self.current_size:
             return index * 2
         else:
@@ -46,15 +46,15 @@ class BinHeap:
             else:
                 return index * 2 + 1    
             
-    def delMin(self):
+    def del_min(self):
         retval = self.heap_list[1]
         self.heap_list[1] = self.heap_list[-1]
         self.current_size = self.current_size - 1
         self.heap_list.pop()
-        self.percDown(1)
+        self.perc_down(1)
         return retval           
     
-    def buildHeap(self,alist):
+    def build_heap(self,alist):
         index = len(alist) // 2
         try:
             if len(alist) > self.max_size:
@@ -63,7 +63,7 @@ class BinHeap:
                 self.current_size = len(alist)
                 self.heap_list = [0] + alist[:]
                 while (index > 0):
-                    self.percDown(index)
+                    self.perc_down(index)
                     index = index - 1    
         except IndexError:
             return("It is impossible to have more than " + str(self.max_size) + (" elements"))
@@ -71,7 +71,7 @@ class BinHeap:
     def size(self):
         return self.current_size
      
-    def isEmpty(self):
+    def is_empty(self):
         return self.current_size == 0
     
     def __str__(self):
