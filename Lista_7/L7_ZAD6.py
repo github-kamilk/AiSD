6e002fd47f1531ca0a54fa27f82023a2a9888f52
github-kamilk <kamil.kowalski51@onet.pl>
2022-01-31 03:24:02 +0100

    Update L7_ZAD6.py

--- Lista_7/L7_ZAD6.py
+++ Lista_7/L7_ZAD6.py
@@ -1,16 +1,16 @@
 from L7_ZAD1_5 import Graph
 
 
-def game_over(location, max_size):
+def game_over(location, missionaries, cannibals):
     if location[0] == 0:
-        if (location[1] == 0 and location[2] == 1) or location[1] != 0:
+        if ((location[1] == 0 and location[2] == 1) or location[1] != 0) and location[1] <= cannibals:
             return False
         else:
             return True
-    elif location[0] == max_size:
+    elif location[0] == missionaries:
         return False
     else:
-        if location[0] < location[1] or max_size - location[0] < max_size - location[1]:
+        if location[0] < location[1] or missionaries - location[0] < cannibals - location[1]:
             return True
         else:
             return False
@@ -24,6 +24,7 @@ def calculate_position(location, move):
     else:
         raise ValueError
 
+
 def move_legal(start, end):
     if start[2] == 0:
         if start[0] >= end[0] and start[1] >= end[1]:
@@ -36,20 +37,22 @@ def move_legal(start, end):
         else:
             return False
 
-def create_graph(max_size):
+
+def create_graph(missionaries, cannibals):
+    max_size = missionaries
     moves_list = [(1, 0), (0, 1), (2, 0), (0, 2), (1, 1)]
     g = Graph()
     for i in range(max_size + 1):
         for j in range(max_size + 1):
             for k in range(2):
-                if not game_over((i, j, k), max_size):
+                if not game_over((i, j, k), missionaries, cannibals):
                     g.add_vertex((i, j, k))
     possible_vertex = list(g.vert_list.keys())[:-1]
 
     for vertex in possible_vertex:
         for moves in moves_list:
             if vertex[2] == 0:
-                moves = (moves[0]*(-1), moves[1]*(-1))
+                moves = (moves[0] * (-1), moves[1] * (-1))
             new_position = calculate_position(vertex, moves)
             if new_position in possible_vertex and move_legal(vertex, new_position):
                 g.add_edge(vertex, new_position)
@@ -63,12 +66,14 @@ def get_path(graph, start, finish):
     else:
         return fastest_ways[finish]
 
+
 def solve(missionaries, cannibals):
     if cannibals > missionaries:
         print("Missionaries were eaten.")
     else:
-        graph = create_graph(missionaries)
+        graph = create_graph(missionaries, cannibals)
         print(get_path(graph, (missionaries, cannibals, 0), (0, 0, 1)))
+
 
 if __name__ == "__main__":
     missionaries = 3
